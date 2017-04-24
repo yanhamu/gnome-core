81b502542d3dbd9b46002f356a1702cd4ba4e4d6
Tomas Kriska <kriskatomas360@gmail.com>
2017-04-24 23:15:00 +0200

    added category tree mock

--- Gnome.Web/ClientApp/app/app.module.ts
+++ Gnome.Web/ClientApp/app/app.module.ts
@@ -1,28 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
+import { TreeModule } from 'ng2-tree';
+
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
+    bootstrap: [AppComponent],
     declarations: [
         AppComponent,
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        CategoryComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        TreeModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'category', component: CategoryComponent},
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ Gnome.Web/ClientApp/app/components/category/category.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component } from '@angular/core';
+import { TreeModel, NodeEvent } from 'ng2-tree';
+
+@Component({
+    selector: 'category',
+    templateUrl: './category.component.html'
+})
+export class CategoryComponent {
+    public tree: TreeModel = {
+        value: 'Programming languages by programming paradigm',
+        children: [
+            {
+                value: 'Object-oriented programming',
+                children: [
+                    { value: 'Java' },
+                    { value: 'C++' },
+                    { value: 'C#' },
+                ]
+            },
+            {
+                value: 'Prototype-based programming',
+                children: [
+                    { value: 'JavaScript' },
+                    { value: 'CoffeeScript' },
+                    { value: 'Lua' },
+                ]
+            }
+        ]
+    };
+
+    public logEvent(e: NodeEvent): void {
+        console.log(e);
+    };
+}
