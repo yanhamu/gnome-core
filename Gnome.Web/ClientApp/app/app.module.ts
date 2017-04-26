0508d61850e1e4fe81eb6cb469fdac9b5b72c1fd
Tomáš Kriška <tomas.kriska@actum.cz>
2017-04-26 11:46:32 +0200

    added charts

--- Gnome.Web/ClientApp/app/app.module.ts
+++ Gnome.Web/ClientApp/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { TreeModule } from 'ng2-tree';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
@@ -9,6 +10,7 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { CategoryComponent } from './components/category/category.component';
+import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
     bootstrap: [AppComponent],
@@ -18,17 +20,20 @@ import { CategoryComponent } from './components/category/category.component';
         CounterComponent,
         FetchDataComponent,
         HomeComponent,
-        CategoryComponent
+        CategoryComponent,
+        ReportComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         TreeModule,
+        ChartsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
-            { path: 'category', component: CategoryComponent},
+            { path: 'category', component: CategoryComponent },
+            { path: 'report', component: ReportComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
