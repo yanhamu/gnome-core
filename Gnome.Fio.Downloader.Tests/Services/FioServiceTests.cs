8c55a43a99054e4094c19e41b31c67621d863689
Tomas Kriska <kriskatomas360@gmail.com>
2017-05-03 21:33:54 +0200

    fixed tests

--- Gnome.Fio.Downloader.Tests/Services/FioServiceTests.cs
+++ Gnome.Fio.Downloader.Tests/Services/FioServiceTests.cs
@@ -3,6 +3,7 @@ using Gnome.Fio.Downloader.Model;
 using Gnome.Fio.Downloader.Services;
 using NSubstitute;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -15,10 +16,12 @@ namespace Gnome.Fio.Downloader.Tests.Services
         {
             var accountId = Guid.NewGuid();
             var documentId = Guid.NewGuid();
+            var stream = File.OpenRead(TestData.GetFilePath("sample-transactions.xml"));
 
             var messaging = Substitute.For<IMessaging>();
             var transactionParser = new TransactionParser();
             var documentHandler = Substitute.For<IDocumentHandler>();
+            documentHandler.Load(Arg.Any<Guid>()).Returns(stream);
 
             var service = new FioService(documentHandler, transactionParser, messaging);
 
---
+++ Gnome.Fio.Downloader.Tests/TestData.cs
@@ -0,0 +1,16 @@
+﻿using Gnome.Fio.Downloader.Tests.Services;
+using System.IO;
+using System.Reflection;
+
+namespace Gnome.Fio.Downloader.Tests
+{
+    public static class TestData
+    {
+        public static string GetFilePath(string filename)
+        {
+            var location = typeof(FioServiceTests).GetTypeInfo().Assembly.Location;
+            var dirPath = Path.GetDirectoryName(location);
+            return Path.Combine(dirPath, filename);
+        }
+    }
+}
